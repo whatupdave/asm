54e3b401a2a1d49c0ce1fd15f190dab559b39ce6
Chris Lloyd <christopher.lloyd@gmail.com>
2013-06-17 14:59:10 -0700

    Initialize the secret token to a insecure default if not set.

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -1,2 +1,2 @@
 # You can use `rake secret` to generate a secure secret key.
-ASM::Application.config.secret_key_base = ENV['SECRET_KEY_BASE']
+ASM::Application.config.secret_key_base = ENV['SECRET_KEY_BASE'] || 'asm-secret'
