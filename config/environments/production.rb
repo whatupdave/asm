89ada84113ddd5cc63f519009cf976e14b7e66a1
Chris Lloyd <christopher.lloyd@gmail.com>
2013-06-17 13:25:00 -0700

    Fall back to compiling assets at runtime.
    Also, doesn't initialize app on precompile (which should make it faster). I've also enabled the user-env-compile flag on Heroku which should help it.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -22,15 +22,16 @@ ASM::Application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  config.assets.compile = true
+
+  # Prevent initializing the app and connecting to the database
+  config.assets.initialize_on_precompile = false
 
   # Generate digests for assets URLs.
   config.assets.digest = true
 
   # Version of your assets, change this if you want to expire all your assets.
   config.assets.version = '1.0'
-
-  config.server_static_assets = true
 
   # Specifies the header that your server uses for sending files.
   # config.action_dispatch.x_sendfile_header = "X-Sendfile" # for apache
