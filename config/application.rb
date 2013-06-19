c07b2e1f195b63d2f41390cdc6a84480c66477b6
Chris Lloyd <christopher.lloyd@gmail.com>
2013-06-19 16:05:29 -0700

    Fixed the double logging bug.
    Turns out that Rails 4 logs to STDOUT anyway so that stuff wasn't needed.

--- config/application.rb
+++ config/application.rb
@@ -14,15 +14,6 @@ module ASM
     # Serve static assets. Heroku needs this for production.
     config.serve_static_assets = false
 
-    # Use a logger that writes to STDOUT. Taken from:
-    # https://github.com/heroku/rails_log_stdout
-    config.logger = Logger.new(STDOUT)
-    log_level_name = (
-      [ENV['LOG_LEVEL'].to_s.upcase, "INFO"] &
-      %w{DEBUG INFO WARN ERROR FATAL UNKNOWN}
-    ).compact.first
-    config.logger.level = Logger.const_get(log_level_name)
-
     config.generators do |g|
       g.orm :active_record
       g.template_engine :haml
